1402a657495d217f85300d016249639fb2b11abb
Steven Rejdukowski <steveo732@gmail.com>
2022-09-14 17:38:45 -0500

    work in progress js

--- js/app.js
+++ js/app.js
@@ -2,65 +2,79 @@
 
 console.log('whats up?');
 
-let visitorname = prompt('What is your name?');
+// let visitorname = prompt('What is your name?');
 
-alert(`Welcome to my site ${visitorname}! So glad you are here! Please answer these next few yes or no questions the best you can!`);
+// alert(`Welcome to my site ${visitorname}! So glad you are here! Please answer these next few yes or no questions the best you can!`);
 
 
-let question1 = prompt('Do I live in Chicago?').toLowerCase();
+// let question1 = prompt('Do I live in Chicago?').toLowerCase();
 
-if (question1 === 'y' || question1 === 'yes') {
-  alert('CORRECTOMUNDO!');
-} else if (question1 === 'n' || question1 === 'no') {
-  alert('WRONG!');
-} else {
-  alert('Not a valid answer');
+// if (question1 === 'y' || question1 === 'yes') {
+//   alert('CORRECTOMUNDO!');
+// } else if (question1 === 'n' || question1 === 'no') {
+//   alert('WRONG!');
+// } else {
+//   alert('Not a valid answer');
+// }
+
+
+// let question2 = prompt('Do I have a mustache?').toLowerCase();
+
+// if (question2 === 'y' || question2 === 'yes') {
+//   alert('CORRECTOMUNDO!');
+// } else if (question2 === 'n' || question2 === 'no') {
+//   alert('WRONG!');
+// } else {
+//   alert('Not a valid answer');
+// }
+
+
+// let question3 = prompt('Is my favorite color green?').toLowerCase();
+
+// if (question3 === 'y' || question3 === 'yes') {
+//   alert('CORRECTOMUNDO!');
+// } else if (question3 === 'n' || question3 === 'no') {
+//   alert('WRONG!');
+// } else {
+//   alert('Not a valid answer');
+// }
+
+// let question4 = prompt('I have no pets').toLowerCase();
+
+// if (question4 === 'n' || question4 === 'no') {
+//   alert('CORRECTOMUNDO!');
+// } else if (question4 === 'y' || question4 === 'yes') {
+//   alert('WRONG!');
+// } else {
+//   alert('Not a valid answer');
+// }
+
+
+// let question5 = prompt('I have never left the country').toLowerCase();
+
+// if (question5 === 'n' || question5 === 'no') {
+//   alert('CORRECTOMUNDO!');
+// } else if (question5 === 'y' || question5 === 'yes') {
+//   alert('WRONG!');
+// } else {
+//   alert('Not a valid answer');
+// }
+
+// alert(`Thanks for playing ${visitorname}! Come back soon!`);
+
+let year = 1982;
+
+let question6 = 0;
+let guess = 4;
+
+while (question6 !== year && guess > 0) {
+  question6 = prompt('Guess What Year The Chart Topping Album Thriller by Michael Jackson was Released');
+  if (question6 > year) {
+    alert('Too high please try again'); guess --;
+  } else if (question6 < year){
+    alert('Too low please try again'); guess --;
+  } else if (question6 === year) {
+    alert('Correct!');
+  }
 }
-
-
-let question2 = prompt('Do I have a mustache?').toLowerCase();
-
-if (question2 === 'y' || question2 === 'yes') {
-  alert('CORRECTOMUNDO!');
-} else if (question2 === 'n' || question2 === 'no') {
-  alert('WRONG!');
-} else {
-  alert('Not a valid answer');
-}
-
-
-let question3 = prompt('Is my favorite color green?').toLowerCase();
-
-if (question3 === 'y' || question3 === 'yes') {
-  alert('CORRECTOMUNDO!');
-} else if (question3 === 'n' || question3 === 'no') {
-  alert('WRONG!');
-} else {
-  alert('Not a valid answer');
-}
-
-let question4 = prompt('I have no pets').toLowerCase();
-
-if (question4 === 'n' || question4 === 'no') {
-  alert('CORRECTOMUNDO!');
-} else if (question4 === 'y' || question4 === 'yes') {
-  alert('WRONG!');
-} else {
-  alert('Not a valid answer');
-}
-
-
-let question5 = prompt('I have never left the country').toLowerCase();
-
-if (question5 === 'n' || question5 === 'no') {
-  alert('CORRECTOMUNDO!');
-} else if (question5 === 'y' || question5 === 'yes') {
-  alert('WRONG!');
-} else {
-  alert('Not a valid answer');
-}
-
-alert(`Thanks for playing ${visitorname}! Come back soon!`);
-
-
 
