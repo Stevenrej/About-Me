b5f79c27051f4bc9f34fecf05f00368d65d7c9f5
Steven Rejdukowski <steveo732@gmail.com>
2022-09-15 12:53:55 -0500

    fixed js error

--- js/app.js
+++ js/app.js
@@ -12,7 +12,7 @@ alert(`Welcome to my site ${visitorname}! So glad you are here! Please answer th
 let question1 = prompt('Do I live in Chicago?').toLowerCase();
 
 if (question1 === 'y' || question1 === 'yes') {
-  alert('CORRECTOMUNDO!'); score ++;
+  alert('CORRECTOMUNDO!'); score++;
 } else if (question1 === 'n' || question1 === 'no') {
   alert('WRONG!');
 } else {
@@ -23,7 +23,7 @@ if (question1 === 'y' || question1 === 'yes') {
 let question2 = prompt('Do I have a mustache?').toLowerCase();
 
 if (question2 === 'y' || question2 === 'yes') {
-  alert('CORRECTOMUNDO!'); score ++;
+  alert('CORRECTOMUNDO!'); score++;
 } else if (question2 === 'n' || question2 === 'no') {
   alert('WRONG!');
 } else {
@@ -34,7 +34,7 @@ if (question2 === 'y' || question2 === 'yes') {
 let question3 = prompt('Is my favorite color green?').toLowerCase();
 
 if (question3 === 'y' || question3 === 'yes') {
-  alert('CORRECTOMUNDO!'); score ++;
+  alert('CORRECTOMUNDO!'); score++;
 } else if (question3 === 'n' || question3 === 'no') {
   alert('WRONG!');
 } else {
@@ -44,7 +44,7 @@ if (question3 === 'y' || question3 === 'yes') {
 let question4 = prompt('I have no pets').toLowerCase();
 
 if (question4 === 'n' || question4 === 'no') {
-  alert('CORRECTOMUNDO!'); score ++;
+  alert('CORRECTOMUNDO!'); score++;
 } else if (question4 === 'y' || question4 === 'yes') {
   alert('WRONG!');
 } else {
@@ -55,7 +55,7 @@ if (question4 === 'n' || question4 === 'no') {
 let question5 = prompt('I have never left the country').toLowerCase();
 
 if (question5 === 'n' || question5 === 'no') {
-  alert('CORRECTOMUNDO!'); score ++;
+  alert('CORRECTOMUNDO!'); score++;
 } else if (question5 === 'y' || question5 === 'yes') {
   alert('WRONG!');
 } else {
@@ -70,11 +70,11 @@ let question6 = 0;
 while (question6 !== year && guess > 0) {
   question6 = parseInt(prompt('Guess What Year The Chart Topping Album Thriller by Michael Jackson was Released, you only have 4 chances'));
   if (question6 > year) {
-    alert('Too high please try again'); guess --;
-  } else if (question6 < year){
-    alert('Too low please try again'); guess --;
+    alert('Too far in the future go back a few years, please try again!'); guess--;
+  } else if (question6 < year) {
+    alert('Too far in the past maybe go a few years forward, please try again!'); guess--;
   } else if (question6 === year) {
-    alert('CORRECT'); score ++;
+    alert('CORRECT'); score++;
   }
 }
 
@@ -83,10 +83,9 @@ if (guess < 1) {
 }
 
 
-// Denis helped me figure out this last question.
+// Dennis helped me figure out this last question.
 
 const band = ['daft punk', 'phoenix', 'billy strings', 'smashing pumpkins', 'prince', 'phoebe bridgers', 'my bloody valentine', 'tycho', 'grateful dead', 'harry styles'];
-
 
 
 
